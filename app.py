0c80b09120da6e61bc575db528bd06f4c10eef25
Gabriel Vinicius <112134365+gabrielxvnx@users.noreply.github.com>
2024-04-15 21:19:26 -0300

    Reformulação do código usando pandas e streamlit

--- app.py
+++ app.py
@@ -1,77 +1,45 @@
 import streamlit as st
-from banco_de_dados import BancoDeDados
-from questionario import Questionario
 from datetime import datetime
+from questionario import DadosSessao
 
-# Instancia o banco de dados e o questionário
-banco = BancoDeDados()
-questionario = Questionario()
+dados_sessao = DadosSessao()
 
-def on_continue_clicked():
-    idade = st.session_state['idade']
-    genero = st.session_state['genero']
-    if not idade or genero not in questionario.respostasvalidas['generos']:
-        st.error('Por favor, preencha sua idade e gênero antes de continuar.')
-    else:
-        # Avança para a próxima parte do formulário
-        st.session_state['introducao_completa'] = True
+st.title('Cadastro de dados')
 
-def introducao_formulario():
-    st.title('Bem-vindo ao Questionário de Saúde')
+genero = st.radio("Selecione seu gênero:", ['Masculino', 'Feminino', 'Outro'])
 
-    # Campo para coletar a idade
-    idade = st.text_input(questionario.perguntas['idade'])
-    st.session_state['idade'] = idade
-    
-    # Verifica se a idade está no intervalo permitido ou é '00'
-    if idade and (idade not in questionario.respostasvalidas['limite_idade']):
-        st.error('Idade inválida. Por favor, insira um valor entre 1 e 100 ou 00 para sair.')
-        return False
-    elif idade == '00':
-        # Aqui você pode definir o que acontece quando '00' é inserido
-        st.stop()
+with st.form(key='meu_formulario', clear_on_submit=True):
+    nome = st.text_input(label='Nome')
+    idade = st.text_input(label='Idade')
+    dormir = st.selectbox('Você dorme entre 6 e 8 horas por dia?', ['Sim', 'Não', 'Talvez'])
+    atv_fisica = st.selectbox('Você faz atividades fisicas regulamente?', ['Sim', 'Não', 'Talvez'])
+    agua = st.selectbox('você consome pelo 3 litros de água diariamente?', ['Sim', 'Não', 'Talvez'])
+    fuma = st.selectbox('Você fuma?', ['Sim', 'Não', 'Talvez'])
+    pergunta_personalizada_1 = ""
+    pergunta_personalizada_2 = ""
 
-    genero = st.radio(questionario.perguntas['genero'], questionario.respostasvalidas['generos'])
-    st.session_state['genero'] = genero
+    if genero == 'Masculino':
+        pergunta_personalizada_1 = st.selectbox('Como homem, você já evitou certos tipos de atividade física por acreditar que não são "apropriados" para o seu gênero?', ['Sim', 'Não', 'Talvez'])
+    elif genero == 'Feminino':
+        pergunta_personalizada_2 = st.selectbox('Como mulher, você sente que sua escolha de atividades físicas é influenciada por preocupações com segurança?', ['Sim', 'Não', 'Talvez'])
 
-    # Botão de continuação com função de callback
-    st.button('Continuar', on_click=on_continue_clicked)
+    submit_button = st.form_submit_button(label='Enviar')
 
-def on_submit_clicked():
-    resposta1 = st.session_state['resposta1']
-    resposta2 = st.session_state['resposta2']
-    resposta3 = st.session_state['resposta3']
-    resposta4 = st.session_state['resposta4']
-    # Coleta a data e hora atuais
-    data_completa = datetime.now()
-    hora = data_completa.strftime('%H:%M:%S')
-    data = data_completa.strftime('%d/%m/%Y')
+    if submit_button:
+        data_completa = datetime.now()
+        data_completa_brasil = data_completa 
+        hora = data_completa_brasil.strftime('%H:%M:%S')
+        data = data_completa_brasil.strftime('%d/%m/%Y')
+        dados_sessao.adicionar_ao_csv(nome, idade, genero, dormir, atv_fisica, agua, fuma, pergunta_personalizada_1, pergunta_personalizada_2, hora, data)
 
-    # Compila todas as respostas
-    respostas = [st.session_state['idade'], st.session_state['genero'], resposta1, resposta2, resposta3, resposta4, hora, data]
-    
-    # Adiciona as respostas ao banco de dados e salva em CSV
-    banco.adicionar_resposta(respostas)
-    banco.salvar_em_csv('respostas_questionario')
-    
-    # Exibe a mensagem de sucesso e reinicia o estado da sessão
-    st.success('Respostas registradas com sucesso!')
-    st.session_state['introducao_completa'] = False
+if st.button('Exportar dados para CSV'):
+    csv = dados_sessao.converter_para_csv(dados_sessao.obter_estado())
+    st.download_button(
+        label="Baixar CSV",
+        data=csv,
+        file_name='dados.csv',
+        mime='text/csv',
+    )
 
-def perguntas_formulario():
-    st.title('Perguntas do Questionário')
-
-    # Campos do formulário de perguntas
-    st.session_state['resposta1'] = st.radio(questionario.perguntas['resposta1'], ('Sim', 'Não', 'Não sei responder'))
-    st.session_state['resposta2'] = st.radio(questionario.perguntas['resposta2'], ('Sim', 'Não', 'Não sei responder'))
-    st.session_state['resposta3'] = st.radio(questionario.perguntas['resposta3'], ('Sim', 'Não', 'Não sei responder'))
-    st.session_state['resposta4'] = st.radio(questionario.perguntas['resposta4'], ('Sim', 'Não', 'Não sei responder'))
-
-    # Botão de envio com função de callback
-    st.button('Enviar', on_click=on_submit_clicked)
-
-# Verifica se a introdução foi completada para mostrar as perguntas
-if 'introducao_completa' not in st.session_state or not st.session_state['introducao_completa']:
-    introducao_formulario()
-else:
-    perguntas_formulario()
+st.write('Dados inseridos:')
+st.write(dados_sessao.obter_estado())
